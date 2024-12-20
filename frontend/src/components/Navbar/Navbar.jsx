e79da4b8bc0fb2857a298056c3d818938f5cfa4e
thanhnguyen-ttn <trungthanhnguyenuit@gmail.com>
2024-12-20 12:05:20 +0700

    fix bug import

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,29 @@
-
-
 import './Navbar.css';
 import { assets } from '../../assets/assets';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
 
-const[menu, setMenu] = useState("menu");
-  
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li className ={menu ==="home"?"active":""}>home</li>
-        <li className ={menu ==="menu"?"active":""}>menu</li>
-        <li className ={menu ==="mobile-app"?"active":""}>mobile-app</li>
-        <li className ={menu ==="contact-us"?"active":""}>contact us</li>
-        </ul>
+        <li className={menu === "home" ? "active" : ""} onClick={() => setMenu("home")}>home</li>
+        <li className={menu === "menu" ? "active" : ""} onClick={() => setMenu("menu")}>menu</li>
+        <li className={menu === "mobile-app" ? "active" : ""} onClick={() => setMenu("mobile-app")}>mobile-app</li>
+        <li className={menu === "contact-us" ? "active" : ""} onClick={() => setMenu("contact-us")}>contact us</li>
+      </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
-        </div> className
+        </div>
         <button>Sign in</button>
       </div>
     </div>
-  )
+  );
 };
 
-export default Navbar
+export default Navbar;
